c9289b520c67a7094abecebc9d410a6dc634a86a
Chris Done <chrisdone@gmail.com>
2013-07-01 18:30:18 +0200

    Prepare for Hackage release.

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
     $ sudo su postgres --command 'createuser hpaste -P'
     $ sudo su postgres --command 'createdb hpaste -O hpaste'
     $ cat sql/schema.sql | psql -U hpaste -h 127.0.0.1 -d hpaste
-    $ cat sql/data.sql | psql -U hpaste -h 127.0.0.1 -d hpaste
 
 ## Configuration & Running
 
-    $ cp amelie.conf.sample amelie.conf
+    $ cp hpaste.conf.sample hpaste.conf
 
-Edit amelie.conf.
+Edit hpaste.conf.
  
-    $ dist/build/amelie/amelie amelie.conf
+    $ dist/build/hpaste/hpaste hpaste.conf
+
--- sql/schema.sql
+++ sql/schema.sql
@@ -4,10 +4,37 @@
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
+SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET escape_string_warning = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
 
 SET search_path = public, pg_catalog;
 
@@ -16,7 +43,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: channel; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: channel; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE channel (
@@ -25,66 +52,58 @@ CREATE TABLE channel (
 );
 
 
-ALTER TABLE public.channel OWNER TO hpaste;
-
 --
--- Name: channel_id_seq; Type: SEQUENCE; Schema: public; Owner: hpaste
+-- Name: channel_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE channel_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
-ALTER TABLE public.channel_id_seq OWNER TO hpaste;
-
 --
--- Name: channel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hpaste
+-- Name: channel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE channel_id_seq OWNED BY channel.id;
 
 
 --
--- Name: hint; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: hint; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE hint (
     id integer NOT NULL,
-    paste integer,
+    paste bigint,
     type text NOT NULL,
     content text NOT NULL
 );
 
 
-ALTER TABLE public.hint OWNER TO hpaste;
-
 --
--- Name: hint_id_seq; Type: SEQUENCE; Schema: public; Owner: hpaste
+-- Name: hint_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE hint_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
-ALTER TABLE public.hint_id_seq OWNER TO hpaste;
-
 --
--- Name: hint_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hpaste
+-- Name: hint_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE hint_id_seq OWNED BY hint.id;
 
 
 --
--- Name: language; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: language; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE language (
@@ -96,35 +115,31 @@ CREATE TABLE language (
 );
 
 
-ALTER TABLE public.language OWNER TO hpaste;
-
 --
--- Name: language_id_seq; Type: SEQUENCE; Schema: public; Owner: hpaste
+-- Name: language_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE language_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
-ALTER TABLE public.language_id_seq OWNER TO hpaste;
-
 --
--- Name: language_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hpaste
+-- Name: language_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE language_id_seq OWNED BY language.id;
 
 
 --
--- Name: paste; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: paste; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE paste (
-    id integer NOT NULL,
+    id bigint NOT NULL,
     title character varying(512) NOT NULL,
     content text NOT NULL,
     tags text,
@@ -133,102 +148,91 @@ CREATE TABLE paste (
     views integer DEFAULT 0 NOT NULL,
     language integer,
     channel integer,
-    annotation_of integer,
+    annotation_of bigint,
     expire timestamp with time zone,
     output text,
     public boolean DEFAULT true,
-    revision_of integer
+    revision_of bigint,
+    spamrating integer DEFAULT 0
 );
 
 
-ALTER TABLE public.paste OWNER TO hpaste;
-
 --
--- Name: paste_id_seq; Type: SEQUENCE; Schema: public; Owner: hpaste
+-- Name: paste_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE paste_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
-ALTER TABLE public.paste_id_seq OWNER TO hpaste;
-
 --
--- Name: paste_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hpaste
+-- Name: paste_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE paste_id_seq OWNED BY paste.id;
 
 
 --
--- Name: public_paste; Type: VIEW; Schema: public; Owner: hpaste
---
-
-CREATE VIEW public_paste AS
-    SELECT paste.id, paste.title, paste.content, paste.author, paste.created, paste.views, paste.language, paste.channel, paste.annotation_of, paste.revision_of FROM paste WHERE paste.public;
-
-
-ALTER TABLE public.public_paste OWNER TO hpaste;
-
--- Name: private_paste
-
-CREATE VIEW private_paste AS
-    SELECT paste.id, paste.title, paste.content, paste.author, paste.created, paste.views, paste.language, paste.channel, paste.annotation_of, paste.revision_of FROM paste WHERE not paste.public;
-
-
-ALTER TABLE public.public_paste OWNER TO hpaste;
-
---
--- Name: public_toplevel_paste; Type: VIEW; Schema: public; Owner: hpaste
---
-
-CREATE VIEW public_toplevel_paste AS
-    SELECT public_paste.id, public_paste.title, public_paste.content, public_paste.author, public_paste.created, public_paste.views, public_paste.language, public_paste.channel, public_paste.annotation_of, public_paste.revision_of FROM public_paste WHERE ((public_paste.annotation_of IS NULL) AND (public_paste.revision_of IS NULL));
-
-
-ALTER TABLE public.public_toplevel_paste OWNER TO hpaste;
-
---
--- Name: report; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: report; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE report (
-    id integer NOT NULL,
-    paste integer NOT NULL,
+    id bigint NOT NULL,
+    paste bigint NOT NULL,
     comments text NOT NULL,
     created timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
-ALTER TABLE public.report OWNER TO hpaste;
+--
+-- Name: private_paste; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW private_paste AS
+    SELECT paste.id, paste.title, paste.content, paste.author, paste.created, paste.views, paste.language, paste.channel, paste.annotation_of, paste.revision_of FROM paste WHERE (paste.id IN (SELECT report.paste FROM report));
+
 
 --
--- Name: report_id_seq; Type: SEQUENCE; Schema: public; Owner: hpaste
+-- Name: public_paste; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW public_paste AS
+    SELECT paste.id, paste.title, paste.content, paste.author, paste.created, paste.views, paste.language, paste.channel, paste.annotation_of, paste.revision_of, paste.spamrating, paste.public FROM paste WHERE (NOT (paste.id IN (SELECT report.paste FROM report)));
+
+
+--
+-- Name: public_toplevel_paste; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW public_toplevel_paste AS
+    SELECT public_paste.id, public_paste.title, public_paste.content, public_paste.author, public_paste.created, public_paste.views, public_paste.language, public_paste.channel, public_paste.annotation_of, public_paste.revision_of, public_paste.spamrating FROM public_paste WHERE (((public_paste.annotation_of IS NULL) AND (public_paste.revision_of IS NULL)) AND public_paste.public);
+
+
+--
+-- Name: report_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE report_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
-ALTER TABLE public.report_id_seq OWNER TO hpaste;
-
 --
--- Name: report_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hpaste
+-- Name: report_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE report_id_seq OWNED BY report.id;
 
 
 --
--- Name: step; Type: TABLE; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: step; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE step (
@@ -238,45 +242,43 @@ CREATE TABLE step (
 );
 
 
-ALTER TABLE public.step OWNER TO hpaste;
-
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: hpaste
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE channel ALTER COLUMN id SET DEFAULT nextval('channel_id_seq'::regclass);
+ALTER TABLE ONLY channel ALTER COLUMN id SET DEFAULT nextval('channel_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: hpaste
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE hint ALTER COLUMN id SET DEFAULT nextval('hint_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: hpaste
---
-
-ALTER TABLE language ALTER COLUMN id SET DEFAULT nextval('language_id_seq'::regclass);
+ALTER TABLE ONLY hint ALTER COLUMN id SET DEFAULT nextval('hint_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: hpaste
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE paste ALTER COLUMN id SET DEFAULT nextval('paste_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: hpaste
---
-
-ALTER TABLE report ALTER COLUMN id SET DEFAULT nextval('report_id_seq'::regclass);
+ALTER TABLE ONLY language ALTER COLUMN id SET DEFAULT nextval('language_id_seq'::regclass);
 
 
 --
--- Name: channel_pkey; Type: CONSTRAINT; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY paste ALTER COLUMN id SET DEFAULT nextval('paste_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY report ALTER COLUMN id SET DEFAULT nextval('report_id_seq'::regclass);
+
+
+--
+-- Name: channel_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY channel
@@ -284,7 +286,7 @@ ALTER TABLE ONLY channel
 
 
 --
--- Name: hint_pkey; Type: CONSTRAINT; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: hint_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY hint
@@ -292,7 +294,7 @@ ALTER TABLE ONLY hint
 
 
 --
--- Name: language_pkey; Type: CONSTRAINT; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: language_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY language
@@ -300,7 +302,7 @@ ALTER TABLE ONLY language
 
 
 --
--- Name: paste_pkey; Type: CONSTRAINT; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: paste_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY paste
@@ -308,7 +310,7 @@ ALTER TABLE ONLY paste
 
 
 --
--- Name: report_pkey; Type: CONSTRAINT; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: report_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY report
@@ -316,28 +318,28 @@ ALTER TABLE ONLY report
 
 
 --
--- Name: paste_author_index; Type: INDEX; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: paste_author_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE INDEX paste_author_index ON paste USING btree (author);
 
 
 --
--- Name: paste_date_index; Type: INDEX; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: paste_date_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE INDEX paste_date_index ON paste USING btree (created);
 
 
 --
--- Name: paste_title_index; Type: INDEX; Schema: public; Owner: hpaste; Tablespace: 
+-- Name: paste_title_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE INDEX paste_title_index ON paste USING btree (title);
 
 
 --
--- Name: hint_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: hint_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY hint
@@ -345,7 +347,7 @@ ALTER TABLE ONLY hint
 
 
 --
--- Name: paste_channel_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: paste_channel_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY paste
@@ -353,7 +355,7 @@ ALTER TABLE ONLY paste
 
 
 --
--- Name: paste_language_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: paste_language_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY paste
@@ -361,7 +363,7 @@ ALTER TABLE ONLY paste
 
 
 --
--- Name: paste_revision_of_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: paste_revision_of_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY paste
@@ -369,7 +371,7 @@ ALTER TABLE ONLY paste
 
 
 --
--- Name: report_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: report_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY report
@@ -377,7 +379,7 @@ ALTER TABLE ONLY report
 
 
 --
--- Name: step_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: hpaste
+-- Name: step_paste_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY step
@@ -385,7 +387,7 @@ ALTER TABLE ONLY step
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: postgres
+-- Name: public; Type: ACL; Schema: -; Owner: -
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
@@ -397,7 +399,4 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 --
 -- PostgreSQL database dump complete
 --
-
-
-ALTER TABLE paste ADD spamrating integer DEFAULT '0';
 
